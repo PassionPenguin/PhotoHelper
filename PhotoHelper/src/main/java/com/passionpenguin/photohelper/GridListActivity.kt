77c484e9c65872120039dd6b59e76285110a04b9
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2020-08-14 12:39:26 +0800

    Initialize Project

---
+++ PhotoHelper/src/main/java/com/passionpenguin/photohelper/GridListActivity.kt
@@ -0,0 +1,11 @@
+package com.passionpenguin.photohelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid_list)
+    }
+}
